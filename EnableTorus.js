985eb9e72b45c72e7bf54d602b0b9bbc6a0b9ff6
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-27 23:23:29 +0900

    Create EnableTorus.js

---
+++ EnableTorus.js
@@ -0,0 +1,19 @@
+
+import Torus from "@toruslabs/torus-embed";
+import Web3 from "web3";
+
+const torus = new Torus({
+  buttonPosition: "top-left" // default: bottom-left
+});
+await torus.init({
+  buildEnv: "production", // default: production
+  enableLogging: true, // default: false
+  network: {
+    host: "kovan", // default: mainnet
+    chainId: 42, // default: 1
+    networkName: "Kovan Test Network" // default: Main Ethereum Network
+  },
+  showTorusButton: false // default: true
+});
+await torus.login(); // await torus.ethereum.enable()
+const web3 = new Web3(torus.provider);
